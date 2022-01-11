63cf215e8be37a2654da3668b02038520b0ca348
Vinícius Silva <viniciusvsom@gmail.com>
2022-01-11 20:05:59 -0300

    Create 01-variaveis.php

---
+++ 01-variaveis/01-variaveis.php
@@ -0,0 +1,16 @@
+<?php
+// nome da variável começa sempre com o $ e um valor
+$nome = "Vinícius da Silva Oliveira Melo";
+$idade = 23;
+$altura = 1.90;
+$carro = "BMW 320i";
+$cidade = "Areado";
+$profissao = "Programador";
+
+// exibindo as variáveis criadas
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura <br>";
+echo "Gostaria de comprar uma $carro <br>";
+echo "Sou da cidade de $cidade <br>";
+echo "Minha profissão é ser $profissao";
+
+?>
