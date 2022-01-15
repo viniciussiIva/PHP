4a169de4990bb93e37a4603f9719d108ba3968d2
Vinícius Silva <viniciusvsom@gmail.com>
2022-01-14 21:29:57 -0300

    Update 01-variaveis.php

--- 01-variaveis/01-variaveis.php
+++ 01-variaveis/01-variaveis.php
@@ -3,13 +3,11 @@
 $nome = "Vinícius da Silva Oliveira Melo";
 $idade = 23;
 $altura = 1.90;
-$carro = "BMW 320i";
 $cidade = "Areado";
 $profissao = "Programador";
 
 // exibindo as variáveis criadas
 echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura <br>";
-echo "Gostaria de comprar uma $carro <br>";
 echo "Sou da cidade de $cidade <br>";
 echo "Minha profissão é ser $profissao";
 
